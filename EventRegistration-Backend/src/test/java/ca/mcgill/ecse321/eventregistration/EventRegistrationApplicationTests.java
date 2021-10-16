3de75cacf24ac1607725e77a4d004ff43dc13d0e
AntonioShen <1053619500@qq.com>
2021-10-16 15:38:04 -0400

    first commit

---
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.eventregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
